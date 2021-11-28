2c5b6e8ee6b7dadb5f1cc683a8cf65ba820dde84
Gustavo Barrientos <gustavo.barrientos@acidstudios.me>
2021-11-28 02:42:46 -0600

    RGB changes on every layer.

--- config.h
+++ config.h
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
+#define TAPPING_TERM 300
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
@@ -43,7 +43,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
-    #define RGBLIGHT_SLEEP // If the host goes to sleet, the keyboard also.
+    #define RGBLIGHT_SLEEP // If the host goes to sleep, the keyboard also.
 #endif
 
 #define OLED_FONT_H "keyboards/crkbd/keymaps/tavobarrientos/glcdfont.c"
--- keymap.c
+++ keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  RSFT_T(KC_ESC),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(1),  LALT_T(KC_SPC),     KC_ENT,   MO(2), RALT_T(KC_DEL)
                                       //`--------------------------'  `--------------------------'
@@ -102,6 +102,31 @@ void oled_render_layer_state(void) {
             oled_write_ln_P(PSTR("Adjust"), false);
             break;
     }
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    uint8_t layer = biton32(state);
+
+    switch (layer) {
+        case 1:
+            rgblight_setrgb(RGB_AZURE);
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+            break;
+        case 2:
+            rgblight_setrgb(RGB_CYAN);
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+            break;
+        case 3:
+            rgblight_setrgb(RGB_CORAL);
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+            break;
+        default:
+            rgblight_setrgb(RGB_MAGENTA);
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING);
+            break;
+    }
+
+    return state;
 }
 
 
